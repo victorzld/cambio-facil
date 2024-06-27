671c70c1a4b4e500970a9594967b91c1bc56056a
Luiz Victor <lvictorandrade@outlook.com>
2024-06-26 23:42:47 -0300

    add form for conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
+import { MoveLeft, MoveRight } from 'lucide-react'
+import imageCoins from '../public/assets/imageCoins.png'
+
 export function App() {
   return (
     <main>
       <div className="text-slate-50 h-screen grid grid-cols-2">
         <div className="bg-zinc-900">
-          coluna 1
+          <div>
+            <h1>Currency Converter</h1>
+            <form>
+              <label htmlFor="enterValue">Enter Amount</label>
+              <input type="number" id='enterValue' />
+            </form>
+
+            <form>
+              <select name="" id="">
+                <option value="">USD</option>
+                <option value="BRL"></option>
+                <option value="EUR">USD</option>
+              </select>
+
+              <div>
+                <MoveRight />
+                <MoveLeft />
+              </div>
+
+              <select name="" id="">
+                <option value="">USD</option>
+                <option value="BRL"></option>
+                <option value="EUR">USD</option>
+              </select>
+            </form>
+
+            <span>
+              1 USD = 5.71 BRL
+            </span>
+
+            <button>
+              Get Exchange Rate
+            </button>
+          </div>
         </div>
-        <div className="bg-blue-700">
-          coluna 2
+
+        <div className="flex items-center justify-center bg-blue-700">
+          <img
+            src={imageCoins}
+            width={700}
+            height={700}
+          />
         </div>
       </div>
     </main>
