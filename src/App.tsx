16f25e46513614e7df8d4e58261af02be36b9093
Luiz Victor <lvictorandrade@outlook.com>
2024-06-27 01:00:09 -0300

    starts styling the conversion form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { MoveLeft, MoveRight } from 'lucide-react'
+import { MoveHorizontal } from 'lucide-react'
 import imageCoins from '../public/assets/imageCoins.png'
 
 export function App() {
   return (
     <main>
       <div className="text-slate-50 h-screen grid grid-cols-2">
-        <div className="bg-zinc-900">
-          <div>
-            <h1>Currency Converter</h1>
-            <form>
+        <div className="flex items-center justify-center bg-zinc-900">
+          <div className='flex flex-col gap-5 items-center border w-[350px] rounded-lg'>
+            <h1 className='font-semibold text-2xl py-6'>Currency Converter</h1>
+
+            <form className='flex flex-col gap-2 w-[300px]'>
               <label htmlFor="enterValue">Enter Amount</label>
-              <input type="number" id='enterValue' />
+              <input
+                type="text"
+                id='enterValue'
+                className='bg-transparent border border-white rounded-lg outline-none py-1 px-2'
+              />
             </form>
 
-            <form>
-              <select name="" id="">
-                <option value="">USD</option>
-                <option value="BRL"></option>
-                <option value="EUR">USD</option>
-              </select>
+            <form className='flex items-center justify-center gap-8'>
 
-              <div>
-                <MoveRight />
-                <MoveLeft />
+              <div className='flex flex-col gap-1'>
+                <label htmlFor="fromCoin" className='text-slate-50'>From</label>
+                <select id='fromCoin' className='px-3 py-1 bg-zinc-900 text-sm'>
+                  <option value="USD"> USD</option>
+                  <option value="BRL">BRL</option>
+                  <option value="EUR">EUR</option>
+                </select>
               </div>
 
-              <select name="" id="">
-                <option value="">USD</option>
-                <option value="BRL"></option>
-                <option value="EUR">USD</option>
-              </select>
+              <div className='mt-7'>
+                <MoveHorizontal />
+              </div>
+
+              <div className='flex flex-col gap-1'>
+                <label htmlFor="toCoin">To</label>
+                <select id='toCoin' className='px-3 py-1 bg-zinc-900 text-sm'>
+                  <option value="USD">USD</option>
+                  <option value="BRL">BRL</option>
+                  <option value="EUR">EUR</option>
+                </select>
+              </div>
             </form>
 
             <span>
