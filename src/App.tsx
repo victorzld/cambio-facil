3891f4fc377f61ce7a164c68bce891bec7840e62
Luiz Victor <lvictorandrade@outlook.com>
2024-06-27 23:55:58 -0300

    style the conversion form

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import imageCoins from '../public/assets/imageCoins.png'
 export function App() {
   return (
     <main>
-      <div className="text-slate-50 h-screen grid grid-cols-2">
+      <div className="text-slate-50 h-screen grid grid-cols-2 bg-zinc-900">
         <div className="flex items-center justify-center bg-zinc-900">
-          <div className='flex flex-col gap-5 items-center border w-[350px] rounded-lg'>
-            <h1 className='font-semibold text-2xl py-6'>Currency Converter</h1>
 
-            <form className='flex flex-col gap-2 w-[300px]'>
+          <div className='flex flex-col gap-5 items-center border w-[350px] rounded-lg py-10'>
+            <h1 className='font-semibold text-2xl'>Currency Converter</h1>
+
+            <form className='flex flex-col gap-2 w-[300px] mt-5'>
               <label htmlFor="enterValue">Enter Amount</label>
               <input
                 type="text"
@@ -18,14 +19,14 @@ export function App() {
               />
             </form>
 
-            <form className='flex items-center justify-center gap-8'>
+            <form className='flex items-center justify-center gap-7'>
 
               <div className='flex flex-col gap-1'>
                 <label htmlFor="fromCoin" className='text-slate-50'>From</label>
-                <select id='fromCoin' className='px-3 py-1 bg-zinc-900 text-sm'>
-                  <option value="USD"> USD</option>
-                  <option value="BRL">BRL</option>
-                  <option value="EUR">EUR</option>
+                <select id='fromCoin' className='border rounded-md px-4 py-2 bg-zinc-900 text-sm'>
+                  <option value="USD" selected>Dolar</option>
+                  <option value="BRL">Real</option>
+                  <option value="EUR">Euro</option>
                 </select>
               </div>
 
@@ -35,25 +36,27 @@ export function App() {
 
               <div className='flex flex-col gap-1'>
                 <label htmlFor="toCoin">To</label>
-                <select id='toCoin' className='px-3 py-1 bg-zinc-900 text-sm'>
-                  <option value="USD">USD</option>
-                  <option value="BRL">BRL</option>
-                  <option value="EUR">EUR</option>
+                <select id='toCoin' className='border rounded-md px-4 py-2 bg-zinc-900 text-sm'>
+                  <option value="USD">Dolar</option>
+                  <option value="BRL" selected>Real</option>
+                  <option value="EUR">Euro</option>
                 </select>
               </div>
             </form>
 
-            <span>
-              1 USD = 5.71 BRL
-            </span>
-
-            <button>
+            <button className='mt-5 py-4 px-10 bg-purple-700 rounded-lg text-lg
+            hover:scale-105 duration-200'
+            >
               Get Exchange Rate
             </button>
+
+            <span className='mt-5 text-sm font-semibold tracking-wider text-slate-300 hover:text-slate-200 duration-200'>
+              1 USD = 5.71 BRL
+            </span>
           </div>
         </div>
 
-        <div className="flex items-center justify-center bg-blue-700">
+        <div className="flex items-center justify-center">
           <img
             src={imageCoins}
             width={700}
