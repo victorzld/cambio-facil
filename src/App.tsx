aaec88eb3383031efcd7d44ad7d32ffcc5dce312
Luiz Victor <lvictorandrade@outlook.com>
2024-07-02 16:58:18 -0300

    adds currency conversion functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,25 @@ import { useState } from 'react'
 
 export function App() {
 
-  const [amount, setaAmount] = useState('')
+  const [amount, setAmount] = useState('')
   const [coinFrom, setCoinFrom] = useState('USD')
   const [coinTo, setCointTo] = useState('BRL')
   const [result, setResult] = useState('')
-  
+
+
+  async function convertCurrency() {
+    const host = 'api.frankfurter.app';
+    fetch(
+      `https://${host}/latest?amount=${amount}&from=${coinFrom}&to=${coinTo}`
+    )
+      .then(resp => resp.json())
+      .then((data) => setResult(data.rates[coinTo].toFixed(2)));
+
+    if (amount === '') {
+      alert('Digite um valor no campo de conversão')
+    }
+  }
+
   return (
     <main className='max-w-[1216px] mx-auto'>
       <div className="text-slate-50 h-screen grid grid-cols-2 bg-zinc-900">
@@ -28,6 +42,9 @@ export function App() {
             <form className='flex flex-col gap-2 w-[300px] mt-5'>
               <label htmlFor="enterValue">Digite o valor</label>
               <input
+                onChange={(e) => setAmount(e.target.value)}
+                value={amount}
+                autoComplete='off'
                 type="text"
                 id='enterValue'
                 className='bg-transparent border border-white rounded-lg outline-none py-1 px-2'
@@ -38,7 +55,12 @@ export function App() {
 
               <div className='flex flex-col gap-1'>
                 <label htmlFor="fromCoin" className='text-slate-50'>De</label>
-                <select id='fromCoin' className='border rounded-md px-4 py-2 bg-zinc-900 text-sm'>
+                <select
+                  onChange={(e) => setCoinFrom(e.target.value)}
+                  value={coinFrom}
+                  id='fromCoin'
+                  className='border rounded-md px-4 py-2 bg-zinc-900 text-sm'
+                >
                   <option value="USD" selected>Dolar</option>
                   <option value="BRL">Real</option>
                   <option value="EUR">Euro</option>
@@ -51,7 +73,12 @@ export function App() {
 
               <div className='flex flex-col gap-1'>
                 <label htmlFor="toCoin">Para</label>
-                <select id='toCoin' className='border rounded-md px-4 py-2 bg-zinc-900 text-sm'>
+                <select
+                  value={coinTo}
+                  onChange={(e) => setCointTo(e.target.value)}
+                  id='toCoin'
+                  className='border rounded-md px-4 py-2 bg-zinc-900 text-sm'
+                >
                   <option value="USD">Dolar</option>
                   <option value="BRL" selected>Real</option>
                   <option value="EUR">Euro</option>
@@ -59,15 +86,21 @@ export function App() {
               </div>
             </form>
 
-            <button className='mt-5 py-4 px-10 bg-purple-700 rounded-lg text-lg
+            <button
+              onClick={() => convertCurrency()}
+              className='mt-5 py-4 px-10 bg-purple-700 rounded-lg text-lg
             hover:scale-105 duration-200 hover:tracking-wide'
             >
               Obter valor convertido
             </button>
 
-            <span className='mt-5 text-sm font-semibold tracking-wider text-slate-300 hover:text-slate-200 duration-200'>
-              1 USD = 5.71 BRL
-            </span>
+            {result.length > 0 && (
+              <span
+                className='mt-5 text-sm font-semibold tracking-wider text-slate-300 hover:text-slate-200 duration-200'
+              >
+                O valor convertido é {result}
+              </span>
+            )}
           </div>
         </div>
 
